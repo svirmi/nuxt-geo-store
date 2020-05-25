415e434eaf569dede5fd43db67670d9a0442d972
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-05-25 08:09:58 +0300

    mutations in store

--- store/index.js
+++ store/index.js
@@ -47,3 +47,36 @@ const state = {
   activeStore: null,
   locations: [],
 };
+
+const createStore = () => {
+  const store = new Vuex.Store({
+    state,
+    mutations: {
+      LOCATIONS_SET(state, locations) {
+        state.locations = locations;
+      },
+      PRODUCTS_SET(state, products) {
+        state.products = products;
+      },
+      POSITION_SET(state, pos) {
+        state.position = pos;
+        state.activeStore = null;
+      },
+      PRICE_SET(state, price) {
+        state.price = price;
+        state.activeStore = null;
+      },
+      RADIUS_SET(state, radius) {
+        state.radius = radius;
+        state.activeStore = null;
+      },
+      SEARCH_TERM_SET(state, searchTerm) {
+        state.searchTerm = searchTerm;
+        state.activeStore = null;
+      },
+      ACTIVE_STORE_SET(state, activeStore) {
+        state.activeStore = activeStore;
+      },
+    },
+  });
+}
